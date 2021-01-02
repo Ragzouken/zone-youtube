ceb38becc8cefd5e41eb5a4ff1a2189bf4c0b451
mark <m.wonnacott@bristol.ac.uk>
2021-01-02 15:11:17 +0000

    initial stuff

---
+++ src/server.js
@@ -0,0 +1,129 @@
+const { nanoid } = require("nanoid");
+const { parse, dirname, join } = require("path");
+const { mkdir } = require("fs").promises;
+
+const express = require("express");
+const ytsr = require('ytsr');
+const joi = require("joi");
+
+require('dotenv').config();
+require('dotenv').config({ path: ".env.defaults" });
+
+mkdir(process.env.MEDIA_PATH).catch(() => {});
+mkdir(dirname(process.env.DATA_PATH)).catch(() => {});
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const db = low(new FileSync(process.env.DATA_PATH, { serialize: JSON.stringify, deserialize: JSON.parse }));
+
+const MEDIA_PATH = process.env.MEDIA_PATH;
+
+process.title = "zone youtube";
+
+db.defaults({
+    entries: [],
+}).write();
+
+const library = new Map(db.get("entries"));
+
+function save() {
+    db.set("entries", Array.from(library)).write();
+}
+
+process.on('SIGINT', () => {
+    save();
+    process.exit();
+});
+
+const app = express();
+app.use(express.json());
+
+/**
+ * @param {express.Request} request 
+ * @param {express.Response} response 
+ * @param {express.NextFunction} next 
+ */
+function requireAuth(request, response, next) {
+    const auth = request.headers.authorization;
+
+    if (auth && auth.startsWith("Bearer") && auth.endsWith(process.env.PASSWORD)) {
+        next();
+    } else if (request.body && request.body.password === process.env.PASSWORD) {
+        next();
+    } else {
+        response.status(401).json({ title: "Invalid password." });
+    }
+}
+
+/**
+ * @param {express.Request} request 
+ * @param {express.Response} response 
+ * @param {express.NextFunction} next 
+ */
+function requireLibraryEntry(request, response, next) {
+    request.libraryEntry = library.get(request.params.id);
+
+    if (request.libraryEntry) {
+        next();
+    } else {
+        response.status(404).json({ title: "Entry does not exist." });
+    }
+}
+
+function getLocalPath(info) {
+    return join(MEDIA_PATH, info.filename);
+}
+
+async function searchYoutube(options) {
+    const result = await ytsr(options.q, { limit: 30 });
+    const videos = result.items.filter((item) => item.type === "video");
+    const entries = videos.map((video) => ({
+        youtubeId: video.id,
+        title: video.title,
+        duration: timeToSeconds(video.duration) * 1000,
+        thumbnail: video.bestThumbnail.url,
+    }));
+    return entries;
+}
+
+app.get("/youtube", async (request, response) => {
+    if (request.query && request.query.q) {
+        let entries = await searchYoutube(request.query || {});
+        response.json(entries);
+    } else {
+        response.status(400).json("Search query q is required.");
+    }
+});
+
+app.get("/youtube/:id", requireLibraryEntry, (request, response) => {
+    // video info
+});
+
+app.post("/youtube/:id", requireAuth, async (request, response) => {
+    // request video
+});
+
+const tagSchema = joi.string().lowercase().min(1).max(32);
+const patchSchema = joi.object({
+    setTitle: joi.string().min(1).max(128),
+    addTags: joi.array().items(tagSchema).default([]),
+    delTags: joi.array().items(tagSchema).default([]),
+});
+
+app.delete("/youtube/:id", requireAuth, requireLibraryEntry, async (request, response) => {
+    save();
+});
+
+const listener = app.listen(process.env.PORT, "localhost", () => {
+    console.log("zone youtube serving on " + listener.address().port);
+});
+
+function timeToSeconds(time) {
+    const parts = time.split(':');
+
+    const seconds = parseInt(parts.pop() || '0', 10);
+    const minutes = parseInt(parts.pop() || '0', 10);
+    const hours = parseInt(parts.pop() || '0', 10);
+
+    return seconds + minutes * 60 + hours * 3600;
+}
